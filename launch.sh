722c226503c69b7c30a9985894ad01beb5dd0a14
Juho Härme <juho.harme@utu.fi>
2019-10-28 16:19:02 +0200

    add git config from env variable

--- launch.sh
+++ launch.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
+# Git config
+if [ ! -z "$GIT_USER_NAME" ] && [ ! -z "$GIT_USER_EMAIL" ]; then
+    git config --global user.name "$GIT_USER_NAME"
+    git config --global user.email "$GIT_USER_EMAIL"
+fi
+
 #sudo chmod -R 777 /home/developer/project
 nvim
